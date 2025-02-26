4c4708c6bc74bf32be8387462e7bc3254c1a20d2
Joao <joao.pparella@senacsp.edu.br>
2025-02-25 22:21:50 -0300

    Alterações - Criando DTO, validações e testes

--- src/usuario/usuario.dm.ts
+++ src/usuario/usuario.dm.ts
@@ -12,4 +12,31 @@ export class UsuariosArmazenados{
     get Usuarios(){        
         return this.#usuarios;
     }
+
+    validaUsuario(dadosUsuario){
+        var validacoes: string[] = [];
+        if(!(dadosUsuario.id != null)){
+            validacoes.push("Id não pode ser nulo");
+        }
+        if(!(dadosUsuario.nome != null)){
+            validacoes.push("Nome não pode ser nulo");
+        }
+        if(!(dadosUsuario.idade != null)){
+            validacoes.push("Idade não pode ser nulo");
+        }
+        if(!(dadosUsuario.cidade != null)){
+            validacoes.push("Cidade não pode ser nulo");
+        }
+        if(!(dadosUsuario.email != null)){
+            validacoes.push("Email não pode ser nulo");
+        }
+        if(!(dadosUsuario.telefone != null)){
+            validacoes.push("Telefone não pode ser nulo");
+        }
+        if(!(dadosUsuario.senha != null)){
+            validacoes.push("Senha não pode ser nulo");        
+        }
+
+        return validacoes;
+    }
 }
