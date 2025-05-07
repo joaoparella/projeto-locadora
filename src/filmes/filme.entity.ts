61b7f958d6cf7ebbfe768c91409d2ebd1fa67a9c
nome <seuemail@email.com>
2025-05-06 22:00:13 -0300

    alterações parciais - criando service do filme

--- src/filmes/filme.entity.ts
+++ src/filmes/filme.entity.ts
@@ -1,18 +1,25 @@
-export class FilmeEntity{
-    id: string;
-    nome: string;
-    duracao: number;
-    sinopse: string;
-    ano: string;
-    genero: string;
-    constructor(id: string,nome: string,duracao: number,sinopse: string,
-        ano: string,genero: string){
-        this.id = id;
-        this.nome = nome;
-        this.duracao = duracao;
-        this.sinopse = sinopse;
-        this.ano = ano;
-        this.genero = genero;
-    }   
+import { GENERO } from "src/genero/genero.entity";
+import { Column, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+
+export class FILME{
+    @PrimaryColumn()
+    ID: string;
+
+    @Column({length:255})
+    NOME: string;
+
+    @Column('int')
+    DURACAO: number;
+
+    @Column({length:255})
+    SINOPSE: string;
+
+    @Column('int')
+    ANO: number;
+
+    @ManyToOne(() => GENERO, genero => genero.filmes)
+    @JoinColumn({name: "IDGENERO", referencedColumnName:"ID"})
+    genero: GENERO;
+
 
 }
