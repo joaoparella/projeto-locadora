0d05bb03a9819c990555627182a33c2545e6c460
nome <seuemail@email.com>
2025-05-06 21:09:54 -0300

    Criação modulo filmes

---
+++ src/filmes/filme.entity.ts
@@ -0,0 +1,18 @@
+export class FilmeEntity{
+    id: string;
+    nome: string;
+    duracao: number;
+    sinopse: string;
+    ano: string;
+    genero: string;
+    constructor(id: string,nome: string,duracao: number,sinopse: string,
+        ano: string,genero: string){
+        this.id = id;
+        this.nome = nome;
+        this.duracao = duracao;
+        this.sinopse = sinopse;
+        this.ano = ano;
+        this.genero = genero;
+    }   
+
+}
