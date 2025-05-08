387fd310ddbcb01846184cb54d7bb049411d62d5
nome <seuemail@email.com>
2025-05-08 20:16:10 -0300

    commit corrigindo erros de compilação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsuarioModule } from './usuario/usuario.module';
+
 import { GeneroModule } from './genero/genero.module';
 import { FilmeModule } from './filmes/filme.module';
 
 
 @Module({
-  imports: [UsuarioModule,GeneroModule,FilmeModule],
+  imports: [GeneroModule,FilmeModule],
   controllers: [],
   providers: [],
 })
