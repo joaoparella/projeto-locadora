6ec6eb1817f816b19d2f9da2d0580cc46cbb071b
nome <seuemail@email.com>
2025-05-08 22:15:15 -0300

    correções modulo de usuário e integração de modulo de arquivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { GeneroModule } from './genero/genero.module';
 import { FilmeModule } from './filmes/filme.module';
 import { PessoaModule } from './pessoa/pessoa.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { FilesModule } from './files/files.module';
 
 
 @Module({
-  imports: [GeneroModule,FilmeModule,PessoaModule],
+  imports: [GeneroModule,FilmeModule,PessoaModule,UsuarioModule,FilesModule],
   controllers: [],
   providers: [],
 })
